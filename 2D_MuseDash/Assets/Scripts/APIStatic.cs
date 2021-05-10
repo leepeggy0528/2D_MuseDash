f0cda9e4be2b23eda1e30fc3928526c7b8d2afaf
leepeggy0528 <peggy6088@gmail.com>
2021-05-10 20:11:41 +0800

    Api code-1

---
+++ 2D_MuseDash/Assets/Scripts/APIStatic.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class APIStatic : MonoBehaviour
+{
+    private void Start()
+    {
+        //屬性-得到
+        print("Random=" + Random.value);
+
+        //屬性-設定
+        Cursor.visible = false;
+
+        //方法
+        int r = Random.Range(50, 100);
+        print("隨機攻擊" + r);
+    }
+}
--- 2D_MuseDash/Assets/Scripts/player.cs
+++ 2D_MuseDash/Assets/Scripts/player.cs
@@ -12,7 +12,7 @@ public class player : MonoBehaviour
     [Header("是否在地上"),Tooltip("可調整位置")]
     public bool flood = false;
 
-    private int score = 0;
+    private int score;
     private AudioClip ac1;
     private AudioClip ac2;
     private AudioSource aus;
@@ -21,6 +21,46 @@ public class player : MonoBehaviour
     #endregion
 
     #region 方式
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+         
+    }
 
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void attack()
+    {
+
+    }
+
+    /// <summary>
+    /// 傷害
+    /// </summary>
+    /// <param name="hurt">受到的傷害</param>
+    private void hit(int hurt)
+    {
+
+    }
+
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    private bool dead()
+    {
+        return false;
+    }
+
+    /// <summary>
+    /// 加分
+    /// </summary>
+    /// <param name="add">要加的分數</param>
+    private void addscore(int add)
+    {
+
+    }
     #endregion
 }
