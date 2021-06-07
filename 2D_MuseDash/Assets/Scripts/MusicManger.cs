ade49c0154a9f119672de322b249af3e1fc64a10
leepeggy0528 <peggy6088@gmail.com>
2021-06-07 20:12:35 +0800

    musicdata

---
+++ 2D_MuseDash/Assets/Scripts/MusicManger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManger : MonoBehaviour
+{
+    [Header("生成物件:上方")]
+    public GameObject objup;
+    [Header("生成物件:下方")]
+    public GameObject objdown;
+    [Header("生成位置:上方")]
+    public GameObject pointup;
+    [Header("生成位置:下方")]
+    public GameObject pointdown;
+}
---
+++ 2D_MuseDash/Assets/Scripts/Musicdata.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="peggy/MusicData", fileName ="MusicgameBase")]
+public class Musicdata : ScriptableObject
+{
+    [Header("音樂開始前的等待時間"),Range(0,10)]
+    public float timeWait=2f;
+    [Header("節點間隔時間"), Range(0, 5)]
+    public float interval = 2f;
+    [Header("節點移動速度"), Range(0, 1000)]
+    public float speed = 300;
+
+    [Header("音樂節點")]
+    public PointType[] points;
+}
+
+///<summary>
+///音樂節點的類型:無節點、上方節點、下方節點、上下節點
+/// </summary>
+public enum PointType
+{
+    none,up,down,both
+}
+
